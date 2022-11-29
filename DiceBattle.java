98fe3032a0187af039000b55f953efdff15aebed
kangmingi20223160 <113087740+kangmingi20223160@users.noreply.github.com>
2022-11-29 23:57:50 +0900

    Update DiceBattle.java

--- DiceBattle.java
+++ DiceBattle.java
@@ -13,19 +13,33 @@ public class DiceBattle {
 			System.out.println(dices[i]);
 		}		
 	}
-	
+	//주사위를 세번 굴려서 배열에 넣는 메서드, 또한 그 배열에 저장된 주사위 3개 출력하는 메서드
 	
 	
 	public static void main(String[] args) {
 		Enemy enemy = new Enemy();
+		PlayerCha p = new PlayerCha();
+		
 		System.out.println("적이 나타났습니다.");
-		enemy.setEnemyHp();
+		enemy.setEnemyHp(); //적 초기 체력 설정
+		p.setPHp(); //플레이어 초기 체력 설정
 		
-		enemy.setEnemyDamage();
-		enemy.check();
-		Rolldice();
+		while(enemy.EHP>0 && p.PHp>0) {
 		
+			enemy.setEnemyDamage();
+			enemy.check();
+			Rolldice();
+		
+		}
 	}
+	
+	
+	// 실행문, 1차적으로 적이 나타났다를 출력, 그 이후에 적의 Hp설정, 데미지 설정한 다음에 몇 HP 몇 damage인지 출력해줍니다.
+	//또한 데미지는 턴마다 반복하여 설정될 예정입니다.
+	//따라서 턴의 순서는 enemy.check(), Rolldice(), 다이스에서 나온 3개 숫자들 공격 방어 회피에 할당시키기, 그리고 턴 종료
+	//턴 종료하면 행동 우선은 항상 플레이어입니다.
+	//플레이어의 공격, 디펜스, 스킬 순서로 할당됩니다.
+	//그 후 적의 HP가 0이하가 되지 않았다면 적은 행동합니다. (오직 공격).
 }
 
 class Enemy{
@@ -37,19 +51,31 @@ class Enemy{
 	void setEnemyHp() {
 		this.EHP = 18 + D6.nextInt(-6,10);
 	}
-	
+//보드 넘버에 따른 추가 체력 줄 예정
 	void setEnemyDamage() {
+		EDamage = 0;
 		this.EDamage = 6 + D6.nextInt(-5,6);
 	}
+//보드 넘버에 따른 추가 데미지 수치 줄 예정
 	
 	void check() {
 		System.out.println("적의 체력: "+ EHP +"\n적의 공격 데미지: "+ EDamage);
 	}
 }
 
-class MyCha {
-	public int MHP;
-	public int MDamage;
-	public int MDefense;
-	public int MSkillnum;
+class PlayerCha {
+	Random D6 = new Random();
+	
+	public int PHp;
+	public int PDamage;
+	public int PDefense;
+	public int PSkillnum;
+	
+	void setPHp() {
+		this.PHp = 20 + D6.nextInt(1,6)*2;
+	}
+	
+//후에 보드 넘버에 따른 추가 체력 계수 줄 예정
+	
+	
 }
